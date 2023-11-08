59581321ad1b1f66cfcd92b43093a86d3a4b240a
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2023-11-08 21:54:47 +0000

    Fix compatibility with other parser plugins

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Injector, common } from "replugged";
 import katex from 'katex';
 import "katex/dist/katex.min.css"
 import { Parser as htmlParserFactory } from "html-to-react"
-import type SimpleMarkdown from "simple-markdown"
+import { ParserRule, ReactOutputRule } from "simple-markdown";
 
 const { parser } = common;
 
@@ -11,46 +11,46 @@ const inject = new Injector();
 export function start(): void {
   const htmlParser = htmlParserFactory({});
 
-  inject.after(parser, "parse", (args) => {
-    return parser.reactParserFor({
-      latex: {
-        order: 23,
-        match(source) {
-          let reg = /^\$(.+?)\$/
-          if (source.startsWith("$$")) {
-            reg = /^\$\$(.+?)\$\$/s
-          }
+  (parser.defaultRules as Record<string, ParserRule & ReactOutputRule>).latex = {
+    order: 23,
+    match(source) {
+      let reg = /^\$(.+?)\$/
+      if (source.startsWith("$$")) {
+        reg = /^\$\$(.+?)\$\$/s
+      }
 
-          return reg.exec(source);
-        },
-        parse(capture) {
-          const content = capture[0]
-          let size = 1
-          if (content.startsWith("$$")) {
-            size = 2
-          }
+      return reg.exec(source);
+    },
+    parse(capture) {
+      const content = capture[0]
+      let size = 1
+      if (content.startsWith("$$")) {
+        size = 2
+      }
 
-          return {
-            content: capture[0].slice(size, -size),
-            type: `latex`,
-            inline: size == 1
-          };
-        },
-        react(node: { content: string, inline: boolean }) {
-          const html = katex.renderToString(node.content, {
-            output: "html",
-            displayMode: !node.inline,
-            throwOnError: false,
-          })
+      return {
+        content: capture[0].slice(size, -size),
+        type: `latex`,
+        inline: size == 1
+      };
+    },
+    react(node) {
+      const html = katex.renderToString(node.content, {
+        output: "html",
+        displayMode: !node.inline,
+        throwOnError: false,
+      })
 
-          return htmlParser.parse(html)
-        },
-      } as SimpleMarkdown.ParserRule,
-      ...parser.defaultRules,
-    })(...args);
-  });
+      return htmlParser.parse(html)
+    },
+  }
+
+  parser.parse = parser.reactParserFor(parser.defaultRules)
 }
 
 export function stop(): void {
   inject.uninjectAll();
+  
+  delete (parser.defaultRules as Record<string, ParserRule & ReactOutputRule>).latex
+  parser.parse = parser.reactParserFor(parser.defaultRules)
 }
